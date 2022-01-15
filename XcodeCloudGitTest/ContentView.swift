5814606faaf4f3425a1b6aff16d8009f5ad76890
Mickaël MAS <appiwedia@gmail.com>
2022-01-15 14:12:08 +0100

    Initial Commit

---
+++ XcodeCloudGitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XcodeCloudGitTest
+//
+//  Created by Mickael Mas on 15/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XcodeCloudGitTest/XcodeCloudGitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudGitTestApp.swift
+//  XcodeCloudGitTest
+//
+//  Created by Mickael Mas on 15/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudGitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
